91267b52d47c7a827342dbea3c31efa920a6f33a
brusnitsyn <andrew.brusnitsyn@gmail.com>
2025-01-09 08:36:44 +0900

    Правка сортировки глав опросника

--- app/Models/SurveyChapter.php
+++ app/Models/SurveyChapter.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class SurveyChapter extends Model
 {
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
+
     protected $fillable = [
         'survey_id',
         'name'
