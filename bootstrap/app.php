5c954101343625f2b81ddda78d421928dc045830
brusnitsyn <brusnitsyn@gmail.com>
2025-07-02 13:26:09 +0900

    Регистрация посредника

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->alias([
+            'hasRole' => \App\Http\Middleware\CheckRole::class,
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
