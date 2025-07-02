8a6e4cd7c7760f0db0b7a61c7a612e981f86283f
brusnitsyn <brusnitsyn@gmail.com>
2025-07-02 11:38:26 +0900

    Добавлен фокус на поле ввода даты

--- resources/js/Components/AppDatePicker.vue
+++ resources/js/Components/AppDatePicker.vue
@@ -2,10 +2,17 @@
 
 import {NDatePicker} from "naive-ui";
 const value = defineModel('value')
+const datePickerRef = ref(null)
+const handleFocus = () => {
+    const input = datePickerRef.value?.$el?.querySelector('input')
+    if (input) input.focus()
+}
 </script>
 
 <template>
     <NDatePicker
+        ref="datePickerRef"
+        @focus="handleFocus"
         v-model:value="value"
         v-mask="'##.##.####'"
         placeholder=""
