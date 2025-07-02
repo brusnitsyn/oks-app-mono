3cced5580bb2e08f12d4d0a75ecbeea1b4b8965f
brusnitsyn <brusnitsyn@gmail.com>
2025-07-02 14:47:14 +0900

    Добавлен столбец "создатель карты" & "МО"

--- database/migrations/2025_07_02_143404_add_create_user_id_column_in_patients_table.php
+++ database/migrations/2025_07_02_143404_add_create_user_id_column_in_patients_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('med_cards', function (Blueprint $table) {
+        Schema::table('patients', function (Blueprint $table) {
             $table->foreignIdFor(\App\Models\User::class, 'creater_user_id')->nullable();
             $table->foreignIdFor(\App\Models\Organization::class, 'organization_id')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('med_cards', function (Blueprint $table) {
+        Schema::table('patients', function (Blueprint $table) {
             $table->dropColumn('creater_user_id');
             $table->dropColumn('organization_id');
         });
